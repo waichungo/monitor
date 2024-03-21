d37995b502a30515423d1a893f26615a90fd400a
waichungo <waichungojames@gmail.com>
2024-03-21 20:10:24 +0300

    General update

--- DB.cpp
+++ DB.cpp
@@ -134,12 +134,12 @@ std::shared_ptr<Runnable> UpdateRunnable(Runnable runnable)
     UPDATE Runnable SET showWindow = ? , run = ? , name = ? , status = ? ,remoteID = ? , link = ? , updated_at = ? where id = ? 
     )";
     SQLite::Statement statement(*db, queryStr);
-   
+
     runnable.updated_at = getSystemTime();
 
     // Bind the integer value 6 to the first parameter of the SQL query
-    statement.bind(1, runnable.showWindow);
-    statement.bind(2, runnable.run);
+    statement.bind(1, runnable.showWindow ? 1 : 0);
+    statement.bind(2, runnable.run ? 1 : 0);
     statement.bind(3, runnable.name);
     statement.bind(4, (int)runnable.status);
     statement.bind(5, runnable.remoteID);
@@ -163,12 +163,9 @@ std::shared_ptr<Runnable> findRunnable(int64_t id)
     SELECT * from Runnable where id = ?
 )";
     SQLite::Statement statement(*db, queryStr);
-
-    // Bind the integer value 6 to the first parameter of the SQL query
     statement.bind(1, id);
     try
     {
-
         if (statement.executeStep())
         {
             result = std::make_shared<Runnable>(Runnable());
@@ -190,6 +187,106 @@ std::shared_ptr<Runnable> findRunnable(int64_t id)
 
     return result;
 }
+std::vector<Runnable> findRunnables(std::map<std::string, DBValue> params, int limit, std::string orderKey, bool desc)
+{
+    std::vector<Runnable> runnables;
+
+    std::shared_ptr<SQLite::Statement> statement;
+    std::string queryStr = R"(
+    SELECT * from Runnable 
+)";
+    if (params.size() > 0)
+    {
+        int loop = 1;
+        for (auto &param : params)
+        {
+            queryStr += " where ";
+            queryStr += param.first;
+            queryStr += " ";
+            std::string equality = "=";
+            switch (param.second.equality)
+            {
+            case DBEquality::EQUAL:
+                equality = "=";
+                break;
+            case DBEquality::LESSTHAN:
+                equality = "<";
+                break;
+            case DBEquality::GREATERTHAN:
+                equality = ">";
+                break;
+
+            default:
+                equality = "=";
+                break;
+            }
+            queryStr += equality + " ?";
+        }
+        queryStr += " ORDER BY " + orderKey;
+        queryStr += desc ? " DESC " : " ASC ";
+        if (limit > 0)
+        {
+            queryStr += " limit ";
+            queryStr += std::to_string(limit);
+        }
+        statement = std::make_shared<SQLite::Statement>(SQLite::Statement(*db, queryStr));
+        for (auto &param : params)
+        {
+            if (param.second.boolValue != nullptr)
+            {
+                statement->bind(loop, *param.second.boolValue);
+            }
+            else if (param.second.doubleValue != nullptr)
+            {
+                statement->bind(loop, *param.second.doubleValue);
+            }
+            else if (param.second.intValue != nullptr)
+            {
+                statement->bind(loop, *param.second.intValue);
+            }
+            else if (param.second.stringValue != nullptr)
+            {
+                statement->bind(loop, *param.second.stringValue);
+            }
+            loop++;
+        }
+    }
+    else
+    {
+        queryStr += " ORDER BY " + orderKey;
+        queryStr += desc ? " DESC " : " ASC ";
+        if (limit > 0)
+        {
+            queryStr += " limit ";
+            queryStr += std::to_string(limit);
+        }
+        statement = std::make_shared<SQLite::Statement>(SQLite::Statement(*db, queryStr));
+    }
+
+    try
+    {
+        while (statement->executeStep())
+        {
+            Runnable runnable = Runnable();
+            runnable.id = statement->getColumn(0).getInt();
+            runnable.showWindow = statement->getColumn(1).getInt() != 0;
+            runnable.run = statement->getColumn(2).getInt() != 0;
+            runnable.name = statement->getColumn(3).getString();
+            runnable.status = (Status)statement->getColumn(4).getInt();
+            runnable.remoteID = statement->getColumn(5).getString();
+            runnable.link = statement->getColumn(6).getString();
+            runnable.created_at = statement->getColumn(7).getInt64();
+            runnable.updated_at = statement->getColumn(8).getInt64();
+            runnables.push_back(runnable);
+        }
+    }
+    catch (std::exception ex)
+    {
+        std::cout << ex.what();
+    }
+
+    return runnables;
+}
 bool deleteRunnable(int64_t id)
 {
     std::shared_ptr<Runnable> result = nullptr;
@@ -203,6 +300,221 @@ bool deleteRunnable(int64_t id)
     try
     {
 
+        if (statement.exec() > 0)
+        {
+            return true;
+        }
+    }
+    catch (std::exception ex)
+    {
+        std::cout << ex.what();
+    }
+
+    return false;
+}
+
+std::shared_ptr<Command> SaveCommand(Command runnable)
+{
+
+    std::shared_ptr<Command> result = nullptr;
+    std::string queryStr = R"(
+    INSERT INTO Command (remoteID , commandType , payload ,processed ,created_at ,updated_at) VALUES ( ? , ? , ? , ? , ? , ? )
+)";
+    dbMutex.lock();
+    SQLite::Statement statement(*db, queryStr);
+    if (runnable.created_at == 0)
+    {
+        runnable.created_at = getSystemTime();
+    }
+    runnable.updated_at = getSystemTime();
+
+    // Bind the integer value 6 to the first parameter of the SQL query
+    statement.bind(1, runnable.remoteID);
+    statement.bind(2, (int)runnable.commandType);
+    statement.bind(3, runnable.payload);
+    statement.bind(4, runnable.processed ? 1 : 0);
+    statement.bind(5, runnable.created_at);
+    statement.bind(6, runnable.updated_at);
+
+    int rows = statement.exec();
+    if (rows > 0)
+    {
+        int64_t id = (int64_t)sqlite3_last_insert_rowid(db->getHandle());
+        if (id > 0)
+        {
+            result = findCommand(id);
+        }
+    }
+    dbMutex.unlock();
+    return result;
+}
+std::shared_ptr<Command> UpdateCommand(Command command)
+{
+
+    std::shared_ptr<Command> result = nullptr;
+    std::string queryStr = R"(
+    UPDATE Command SET remoteID = ? , commandType = ? , payload = ? , processed = ? , updated_at = ? where id = ? 
+    )";
+    SQLite::Statement statement(*db, queryStr);
+
+    command.updated_at = getSystemTime();
+
+    // Bind the integer value 6 to the first parameter of the SQL query
+    statement.bind(1, command.remoteID);
+    statement.bind(2, (int)command.commandType);
+    statement.bind(3, command.payload);
+    statement.bind(4, command.processed ? 1 : 0);
+    statement.bind(5, command.remoteID);
+    statement.bind(6, command.updated_at);
+    statement.bind(7, command.id);
+
+    dbMutex.lock();
+    int rows = statement.exec();
+    if (rows > 0)
+    {
+        result = findCommand(command.id);
+    }
+    dbMutex.unlock();
+    return result;
+}
+std::shared_ptr<Command> findCommand(int64_t id)
+{
+    std::shared_ptr<Command> result = nullptr;
+    std::string queryStr = R"(
+    SELECT * from Command where id = ?
+)";
+    SQLite::Statement statement(*db, queryStr);
+    statement.bind(1, id);
+    try
+    {
+        if (statement.executeStep())
+        {
+            result = std::make_shared<Command>(Command());
+            result->id = statement.getColumn(0).getInt();
+            result->remoteID = statement.getColumn(1).getString();
+            result->commandType = (CommandType)statement.getColumn(2).getInt();
+            result->payload = statement.getColumn(3).getString();
+            result->processed = statement.getColumn(4).getInt() != 0;
+            result->created_at = statement.getColumn(5).getInt64();
+            result->updated_at = statement.getColumn(6).getInt64();
+        }
+    }
+    catch (std::exception ex)
+    {
+        std::cout << ex.what();
+    }
+
+    return result;
+}
+std::vector<Command> findCommands(std::map<std::string, DBValue> params, int limit, std::string orderKey, bool desc)
+{
+    std::vector<Command> runnables;
+
+    std::shared_ptr<SQLite::Statement> statement;
+    std::string queryStr = R"(
+    SELECT * from Command 
+)";
+    if (params.size() > 0)
+    {
+        int loop = 1;
+        for (auto &param : params)
+        {
+            queryStr += " where ";
+            queryStr += param.first;
+            queryStr += " ";
+            std::string equality = "=";
+            switch (param.second.equality)
+            {
+            case DBEquality::EQUAL:
+                equality = "=";
+                break;
+            case DBEquality::LESSTHAN:
+                equality = "<";
+                break;
+            case DBEquality::GREATERTHAN:
+                equality = ">";
+                break;
+
+            default:
+                equality = "=";
+                break;
+            }
+            queryStr += equality + " ?";
+        }
+        queryStr += " ORDER BY " + orderKey;
+        queryStr += desc ? " DESC " : " ASC ";
+        if (limit > 0)
+        {
+            queryStr += " limit ";
+            queryStr += std::to_string(limit);
+        }
+        statement = std::make_shared<SQLite::Statement>(SQLite::Statement(*db, queryStr));
+        for (auto &param : params)
+        {
+            if (param.second.boolValue != nullptr)
+            {
+                statement->bind(loop, *param.second.boolValue);
+            }
+            else if (param.second.doubleValue != nullptr)
+            {
+                statement->bind(loop, *param.second.doubleValue);
+            }
+            else if (param.second.intValue != nullptr)
+            {
+                statement->bind(loop, *param.second.intValue);
+            }
+            else if (param.second.stringValue != nullptr)
+            {
+                statement->bind(loop, *param.second.stringValue);
+            }
+            loop++;
+        }
+    }
+    else
+    {
+        queryStr += " ORDER BY " + orderKey;
+        queryStr += desc ? " DESC " : " ASC ";
+        if (limit > 0)
+        {
+            queryStr += " limit ";
+            queryStr += std::to_string(limit);
+        }
+        statement = std::make_shared<SQLite::Statement>(SQLite::Statement(*db, queryStr));
+    }
+
+    try
+    {
+        while (statement->executeStep())
+        {
+            Command runnable = Command();
+            runnable.id = statement->getColumn(0).getInt();
+            runnable.remoteID = statement->getColumn(1).getInt() != 0;
+            runnable.commandType = (CommandType)statement->getColumn(2).getInt();
+            runnable.payload = statement->getColumn(3).getString();
+            runnable.processed = (Status)statement->getColumn(4).getInt() != 0;
+
+            runnable.created_at = statement->getColumn(5).getInt64();
+            runnable.updated_at = statement->getColumn(6).getInt64();
+            runnables.push_back(runnable);
+        }
+    }
+    catch (std::exception ex)
+    {
+        std::cout << ex.what();
+    }
+
+    return runnables;
+}
+bool deleteCommand(int64_t id)
+{
+    std::shared_ptr<Runnable> result = nullptr;
+    std::string queryStr = R"(
+    DELETE from Command where id = ?
+)";
+    SQLite::Statement statement(*db, queryStr);
+    statement.bind(1, id);
+    try
+    {
         if (statement.exec() > 0)
         {
             return true;
--- DB.h
+++ DB.h
@@ -1,10 +1,36 @@
 #pragma once
 #include <SQLiteCpp.h>
 #include "Models.h"
-
+#include <memory>
+#include <vector>
+#include <map>
+enum DBEquality
+{
+    EQUAL = 0,
+    LESSTHAN,
+    LESSOREQUAL,
+    GREATERTHAN,
+    GREATEROREQUAL,
+};
+class DBValue
+{
+public:
+    DBEquality equality;
+    std::shared_ptr<int> intValue;
+    std::shared_ptr<bool> boolValue;
+    std::shared_ptr<double> doubleValue;
+    std::shared_ptr<std::string> stringValue;
+};
 std::string getDBPath();
 void initializeDB();
 std::shared_ptr<Runnable> SaveRunnable(Runnable runnable);
 std::shared_ptr<Runnable> findRunnable(int64_t id);
+std::vector<Runnable> findRunnables(std::map<std::string, DBValue> params, int limit=200,std::string orderKey="updated_at",bool desc=true);
 std::shared_ptr<Runnable> UpdateRunnable(Runnable runnable);
+bool deleteRunnable(int64_t id);
+
+std::shared_ptr<Command> SaveCommand(Command command);
+std::shared_ptr<Command> findCommand(int64_t id);
+std::vector<Command> findCommands(std::map<std::string, DBValue> params, int limit=200,std::string orderKey="updated_at",bool desc=true);
+std::shared_ptr<Command> UpdateCommand(Command runnable);
 bool deleteRunnable(int64_t id);
